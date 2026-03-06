9e79cf29fd53f4d5e947f4e16e983cdc60c917d3
Nabil Driouich <97213219+NABILDR2O18@users.noreply.github.com>
2026-03-06 15:21:30 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   preview: {
-    allowedHosts: ["adloot.io"],
+    allowedHosts: ["all"],
   },
   plugins: [react()].filter(Boolean),
   resolve: {
